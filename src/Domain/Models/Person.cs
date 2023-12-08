aec291eadac78b23f7cc16b80eaef9ad2f564848
Gerald Ncube <gerald.ncube@se.com>
2023-12-08 22:26:09 +0000

    Add Age property to Person model

--- src/Domain/Models/Person.cs
+++ src/Domain/Models/Person.cs
@@ -5,7 +5,7 @@ public class Person
     public int Id { get; set; }
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
-    //public int Age { get; set; }
+    public int Age { get; set; }
     public List<Address> Addresses { get; set; } = new();
     public List<Email> EmailAddresses { get; set; } = new();
 
